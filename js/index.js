b76a4239c4b9c7156ad7bd2f055c6da210cc0b1f
Janice-M <janiceink001@gmail.com>
2020-08-06 01:36:17 +0300

    adding the table

--- js/index.js
+++ js/index.js
@@ -89,4 +89,40 @@ $(document).ready(function() {
         $("#outfit").append(newRow);
 
 });
+$(".btn.check-out").click(function() {
+    $(".btn.add-Outfit").hide();
+    $(".btn.check-out").hide();
+    $(".additional-info").show();
+    $(".btn.yes").show();
+    $(".btn.no").show();
+    $(".additional-info .location").hide();
+    grandTotal = grandTotal + total;
+
+    $(".additional-info h3 span").html(grandTotal);
+  });
+
+  $(".btn.yes").click(function() {
+    $(".additional-info h5").hide();
+    $(".btn.yes").hide();
+    $(".btn.no").hide();
+    $(".additional-info .location").show();
+    $(".additional-info h3 span").html(grandTotal + 200);
+  });
+
+  $(".btn.no").click(function() {
+    $(".additional-info h5").hide();
+    $(".btn.yes").hide();
+    $(".btn.no").hide();
+    $(".additional-info .location").show();
+  });
+
+  $(".btn.complete").click(function() {
+    var location = $(".additional-info .location input").val();
+    $(".additional-info h4").show();
+    $(".additional-info .location").hide();
+    $(".additional-info h4 span").html(location);
+  });
+
+});
+
 });
